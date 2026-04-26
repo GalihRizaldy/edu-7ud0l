1fd3c48099fb0305db13513d565024760f47ec40
galih <galihrizaldi47@gmail.com>
2026-04-26 22:05:35 +0700

    perbaikan

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-// Default connection config without specifying database yet
 const poolConfig = {
   host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASS || '',
 };
